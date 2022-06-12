32203679d515998409a65e680fe8499115ee2e3c
Iliya Kostadinov <iliyakostadinov@users.noreply.github.com>
2022-06-12 13:22:07 +0300

    Setup project

---
+++ src/main/java/iliyakostadinov/populationgrowth/app/PopulationGrowthApplication.java
@@ -0,0 +1,13 @@
+package iliyakostadinov.populationgrowth.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PopulationGrowthApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PopulationGrowthApplication.class, args);
+	}
+
+}
