aee6690eed38a637d19a75d20d09a80121c41171
Iliya Kostadinov <iliyakostadinov@users.noreply.github.com>
2022-06-18 14:40:08 +0300

    Add CSV parsing

--- src/main/java/iliyakostadinov/populationgrowth/PopulationGrowthApplication.java
+++ src/main/java/iliyakostadinov/populationgrowth/PopulationGrowthApplication.java
@@ -1,4 +1,4 @@
-package iliyakostadinov.populationgrowth.app;
+package iliyakostadinov.populationgrowth;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
